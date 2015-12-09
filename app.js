7affbc045450862830cbcca19557076aff084d35
allenkung <mrgongxue+github@gmail.com>
2015-12-09 18:40:21 +0800

    level04

--- app.js
+++ app.js
@@ -5,14 +5,48 @@
     this.products = gems;
   });
 
-  app.controller("PanelController", function(){
-    this.tab = 1;
-    // window.alert(this.tab);
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
+  // app.controller("PanelController", function(){
+  //   this.tab = 1;
+  //   // window.alert(this.tab);
+  //   this.selectTab = function(setTab) {
+  //     this.tab = setTab;
+  //   };
+  //   this.isSelected = function(checkTab) {
+  //     return this.tab === checkTab;
+  //   };
+  // });
+
+  app.controller("ReviewController", function(){
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+      this.review = {};
     };
-    this.isSelected = function(checkTab) {
-      return this.tab === checkTab;
+  });
+
+  app.directive('productTitle', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-title.html'
+    };
+  });
+
+  app.directive('productPanels', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller: function(){
+          this.tab = 1;
+          // window.alert(this.tab);
+          this.selectTab = function(setTab) {
+            this.tab = setTab;
+          };
+          this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+          };
+      },
+      controllerAs: 'panels'
     };
   });
 
@@ -29,6 +63,18 @@
         {
           full: 'dodecahedron-02-full.jpg',
           thumb: 'dodecahedron-02-thumb.jpg'
+        }
+      ],
+      reviews: [
+        {
+          stars: 5,
+          body: "I love this product!", 
+          author: "joe@thomas.com" ,
+        },
+        {
+          stars: 1,
+          body: "This product sucks", 
+          author: "tim@thomas.com" ,
         }
       ],
       tag: 'tag 1',
